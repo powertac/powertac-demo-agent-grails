29d7a315ec37f193c710deb948b6b5e124731386
ddauer <ddauer@gmail.com>
2011-03-22 17:16:15 +0100

    Update BuildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,30 +7,36 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.plugin.location.PowertacCommon = "../powertac-common"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
+  // inherit Grails' default dependencies
+  inherits("global") {
+    // uncomment to disable ehcache
+    // excludes 'ehcache'
+  }
+  log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+  repositories {
+    grailsPlugins()
+    grailsHome()
+    grailsCentral()
 
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        // mavenLocal()
-        mavenCentral()
-        // mavenRepo "http://snapshots.repository.codehaus.org"
-        // mavenRepo "http://repository.codehaus.org"
-        // mavenRepo "http://download.java.net/maven/2/"
-        // mavenRepo "http://repository.jboss.com/maven2/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+    // uncomment the below to enable remote dependency resolution
+    // from public Maven repositories
+    // mavenLocal()
+    mavenCentral()
+    mavenRepo "http://snapshots.repository.codehaus.org"
+    mavenRepo "http://repository.codehaus.org"
+    mavenRepo "http://download.java.net/maven/2/"
+    mavenRepo "http://repository.jboss.com/maven2/"
+  }
+  dependencies {
+    // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        // runtime 'mysql:mysql-connector-java:5.1.13'
-		compile 'org.apache.activemq:activemq-core:5.4.2'
+    // runtime 'mysql:mysql-connector-java:5.1.13'
+    compile('org.apache.activemq:activemq-core:5.4.2') {
+      excludes 'commons-collections', 'commons-logging', 'junit', 'log4j', 'spring-context', 'spring-parent', 'spring-aop', 'spring-asm', 'spring-beans', 'spring-expression', 'xalan', 'xml-apis'
     }
+
+    compile('org.apache.activemq:activemq-pool:5.4.2') {
+      excludes 'commons-collections', 'commons-pool', 'commons-logging', 'junit', 'log4j', 'spring-context', 'spring-parent', 'spring-aop', 'spring-asm', 'spring-beans', 'spring-expression', 'xalan', 'xml-apis'
+    }
+  }
 }
