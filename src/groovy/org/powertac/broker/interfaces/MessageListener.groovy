3e187979b5835b6c57da8bdfbe31e9e7c111cade
Nguyen Nguyen <nguyen.a.nguyen@gmail.com>
2011-06-07 15:48:22 -0500

    fix typo in MessageListener

--- src/groovy/org/powertac/broker/interfaces/MessageListener.groovy
+++ src/groovy/org/powertac/broker/interfaces/MessageListener.groovy
@@ -1,13 +1,5 @@
 package org.powertac.broker.interfaces
 
-/**
- * Created by IntelliJ IDEA.
- * User: nnguyen
- * Date: 6/6/11
- * Time: 8:12 PM
- * To change this template use File | Settings | File Templates.
- */
 public interface MessageListener
 {
-
 }
