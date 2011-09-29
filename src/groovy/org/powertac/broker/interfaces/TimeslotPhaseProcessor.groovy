d57e9517685b963da2d43ce4a10f52a7cf6b4127
Nguyen Nguyen <nguyen@entagen.com>
2011-09-28 22:47:38 -0500

    Remove dependency on powertac-server-interface

---
+++ src/groovy/org/powertac/broker/interfaces/TimeslotPhaseProcessor.groovy
@@ -0,0 +1,12 @@
+package org.powertac.broker.interfaces
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nnguyen
+ * Date: 9/28/11
+ * Time: 10:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface TimeslotPhaseProcessor {
+
+}
