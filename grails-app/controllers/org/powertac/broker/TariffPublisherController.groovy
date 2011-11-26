1faf3f19932e2d493cc85933d543c1835fc46ce3
Nguyen Nguyen <nguyen@entagen.com>
2011-11-25 23:39:53 -0600

    Fix publishing tariff spec from web UI

--- grails-app/controllers/org/powertac/broker/TariffPublisherController.groovy
+++ grails-app/controllers/org/powertac/broker/TariffPublisherController.groovy
@@ -6,6 +6,7 @@ import org.powertac.common.TariffSpecification
 import org.powertac.common.enumerations.PowerType
 import org.joda.time.DateTime
 import org.joda.time.DateTimeZone
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
 class TariffPublisherController {
   def tariffPublishingService
@@ -14,45 +15,29 @@ class TariffPublisherController {
     render(view: 'index')
   }
 
+	def create = {
+		render(view: 'index')
+	}
+
   def publish = {
-    def tariffSpec = new TariffSpecification()
+	  def username = ConfigurationHolder.config.powertac.username
+    TariffSpecification tariffSpec = new TariffSpecification(new Broker(username), PowerType.CONSUMPTION)
     bindData(tariffSpec, params, [include: ['minDuration', 'earlyWithdrawPayment', 'powerType', 'periodicPayment', 'signupPayment']])
 
-    log.debug("Username: ${params.username}")
-    tariffSpec.broker = new Broker(username: params.broker, password: "")
-
     if (tariffSpec.powerType == PowerType.CONSUMPTION) { 
-      Rate rate = new Rate()
-      rate.setValue(0.08)
-      rate.setDailyBegin(23)
-      rate.setDailyEnd(6)
-      rate.save()
-      tariffSpec.addToRates(rate)
+      Rate rate = new Rate().withValue(0.08).withDailyBegin(23).withDailyEnd(6)
+      tariffSpec.addRate(rate)
     
-      rate = new Rate()
-      rate.setValue(0.14)
-      rate.setDailyBegin(7)
-      rate.setDailyEnd(22)
-      rate.save()
-      tariffSpec.addToRates(rate)
+      rate = new Rate().withValue(0.14).withDailyBegin(7).withDailyEnd(22)
+      tariffSpec.addRate(rate)
     }
     else if (tariffSpec.powerType == PowerType.PRODUCTION) {
-      Rate rate = new Rate()
-      rate.setValue(-0.05)
-      rate.save()
-      tariffSpec.addToRates(rate)
+      Rate rate = new Rate().withValue(-0.05)
+      tariffSpec.addToRate(rate)
     }
 
-    if (!tariffSpec.validate()) {
-      tariffSpec.save()
-      render(view: 'index', model: [tariffSpecificationInstance: tariffSpec])
-
-    } else {
-
-      tariffPublishingService.publish(tariffSpec)
-
-      flash.message = 'Published tariffSpec'
-      render(view: 'index')
-    }
+		tariffPublishingService.publish(tariffSpec)
+		flash.message = 'Published tariffSpec'
+		render(view: 'index')
   }
 }
