a6415acae22f033789ea07c14c715afb95db9f39
John Collins <jcollins@cs.umn.edu>
2012-01-05 13:28:20 -0600

    update dependency to common 0.2.0-SNAPSHOT

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -25,7 +25,7 @@ grails.project.dependency.resolution = {
     mavenRepo "http://repository.codehaus.org"
     mavenRepo "http://download.java.net/maven/2/"
     mavenRepo "http://repository.jboss.com/maven2/"
-    mavenRepo "http://tac04.cs.umn.edu:8080/plugin/repository/everything"
+    //mavenRepo "http://tac04.cs.umn.edu:8080/plugin/repository/everything"
   }
   dependencies {
     // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
@@ -43,7 +43,7 @@ grails.project.dependency.resolution = {
       excludes 'groovy', 'xml-apis'
     }
 
-    compile 'org.powertac:common:0.1.0'
+    compile 'org.powertac:common:0.2.0-SNAPSHOT'
 
     // this should have been inherited from powertac-common but somehow IntelliJ doesn't see this
     // compile( group: 'com.thoughtworks.xstream', name: 'xstream', version: '1.3.1', export: true )
